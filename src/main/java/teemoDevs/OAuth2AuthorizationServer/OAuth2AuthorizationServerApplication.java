f1bb8f929a34862b46e8d7a80d99b6d62bc1bf72
yechanpark <qkrdpcks0419@naver.com>
2019-05-04 02:02:14 +0900

    OAuth2AuthorizationServer 구현중

--- src/main/java/teemoDevs/OAuth2AuthorizationServer/OAuth2AuthorizationServerApplication.java
+++ src/main/java/teemoDevs/OAuth2AuthorizationServer/OAuth2AuthorizationServerApplication.java
@@ -2,7 +2,9 @@ package teemoDevs.OAuth2AuthorizationServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
+@EnableAuthorizationServer
 @SpringBootApplication
 public class OAuth2AuthorizationServerApplication {
 
