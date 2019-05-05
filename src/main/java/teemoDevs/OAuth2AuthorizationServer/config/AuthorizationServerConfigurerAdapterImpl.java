86f8f462222cfc1b765ac5e0d108cce705505535
ecovaci <eugen.covaci.q@gmail.com>
2019-05-05 10:06:21 +0300

    Fix OAUTH2

--- src/main/java/teemoDevs/OAuth2AuthorizationServer/config/AuthorizationServerConfigurerAdapterImpl.java
+++ src/main/java/teemoDevs/OAuth2AuthorizationServer/config/AuthorizationServerConfigurerAdapterImpl.java
@@ -1,33 +1,44 @@
 package teemoDevs.OAuth2AuthorizationServer.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.approval.UserApprovalHandler;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfigurerAdapterImpl extends AuthorizationServerConfigurerAdapter {
 
+    @Autowired
+    private TokenStore tokenStore;
+
+    @Autowired
+    private UserApprovalHandler userApprovalHandler;
+
+    @Autowired
     private AuthenticationManager authenticationManager;
 
-    public AuthorizationServerConfigurerAdapterImpl(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        this.authenticationManager = authenticationConfiguration.getAuthenticationManager();
-    }
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
                 .withClient("foo")
-                .secret("bar")
+                .secret(passwordEncoder.encode("bar"))
                 //.authorities("USER")
-                .authorizedGrantTypes("authorization_code", "implicit", "refresh_token")
+                .authorizedGrantTypes("authorization_code", "password", "implicit", "refresh_token")
                 .autoApprove(true)
-                .redirectUris("http://localhost:8080/oauth2/authorization/teemo")
+                .redirectUris("http://localhost:8080/client/login/oauth2/code/teemo")
                 .scopes("read");
     }
 
@@ -40,7 +51,8 @@ public class AuthorizationServerConfigurerAdapterImpl extends AuthorizationServe
     public void configure(AuthorizationServerSecurityConfigurer oauthServer)throws Exception {
         oauthServer
                 .tokenKeyAccess("permitAll()")
-                .checkTokenAccess("isAuthenticated()");
+                .checkTokenAccess("isAuthenticated()")
+                .allowFormAuthenticationForClients();;
         // Token 정보를 API(/oauth/check_token)를 활성화 시킨다. ( 기본은 denyAll )
     }
 }
