9d53b9a3020a63ab55d1ebbc58b4c56c0377e104
yechanpark <qkrdpcks0419@naver.com>
2019-05-10 17:36:39 +0900

    미사용 도메인 객체 제거 + 로그아웃 컨트롤러 구현

---
+++ src/main/java/teemoDevs/OAuth2AuthorizationServer/endpoints/LogoutController.java
@@ -0,0 +1,24 @@
+package teemoDevs.OAuth2AuthorizationServer.endpoints;
+
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+public class LogoutController {
+    @GetMapping("/logout")
+    public void exit(HttpServletRequest request, HttpServletResponse response) {
+        // token can be revoked here if needed
+        new SecurityContextLogoutHandler().logout(request, null, null);
+        try {
+            //sending back to client app
+            response.sendRedirect(request.getHeader("referer"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/teemoDevs/OAuth2AuthorizationServer/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package teemoDevs.OAuth2AuthorizationServer.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import teemoDevs.OAuth2AuthorizationServer.auth.User;
-
-@RepositoryRestResource
-public interface UserRepository extends PagingAndSortingRepository<User, Long> {
-}
